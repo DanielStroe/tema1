9d1ba9a64faf1ce99df665027af8e0ad1ab99605
Daniel Stroe <stroe.daniel@yahoo.com>
2013-12-10 00:44:39 +0200

    Tema 1

---
+++ Project2.dpr
@@ -0,0 +1,12 @@
+program Project2;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+begin
+  { TODO -oUser -cConsole Main : Insert code here }
+  Writeln('Daniel - tema nr 1');
+  Readln; //pt a ramane fereastra deschisa  pana la apasarea tastei enter
+end.
